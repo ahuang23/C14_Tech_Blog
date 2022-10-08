9b94698db1f85b9d4436cc78e11a6b6b11c550aa
Alan Huang <a.huang232@gmail.com>
2022-10-08 16:27:05 -0700

    Create models

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+// User one to many relationship to Post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// User one to many relationship to Comment
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+// Post one to one relationship with User
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Post one to many relationship with Comment
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+// Comment one to one relationship with User
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Comment one to one relationship with Post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+
